ebd237fcaaa3c24105cc29db594344dcacbb5274
Tebin <tebin.raouf@gmail.com>
2022-09-26 00:51:35 -0400

    add custom event

--- src/components/h1/h1.template.ts
+++ src/components/h1/h1.template.ts
@@ -2,7 +2,7 @@ import { html } from "@microsoft/fast-element";
 
 
 export const template = html`
-    <h1 style="color: ${x => x.color}">
+    <h1 style="color: ${x => x.color}" @click="${x => x.handler()}">
         <slot></slot>
     </h1>
 `;
--- src/components/h1/h1.ts
+++ src/components/h1/h1.ts
@@ -5,6 +5,13 @@ import { template } from "./h1.template.js";
 
 export class H1Component extends FASTElement {
     @attr color: string = 'brown'
+
+    private handler() {
+        const quotes = ["We cannot solve our problems with the same thinking we used when we created them", "The true sign of intelligence is not knowledge but imagination.", "I have no special talent. I am only passionately curious.", "The only reason for time is so that everything doesn't happen at once.", "Only a life lived for others is a life worthwhile."];
+        const random = Math.floor(Math.random() * quotes.length);
+
+        this.$emit('colorhandler', quotes[random])
+    };
 }
 
 export const h1component = H1Component.compose({
